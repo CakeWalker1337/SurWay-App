b21ff9a3422b5194b3b434b236c00adda608f1de
Maxim Kovalev <max.kovalev15@gmail.com>
2019-11-06 19:32:08 +0700

    Initial commit

---
+++ app/src/main/java/com/tenxgames/surway/auth/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.tenxgames.surway.auth.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
